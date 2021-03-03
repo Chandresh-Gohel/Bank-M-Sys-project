10ffebaa76639b32429c5a6b2732d0f44d84f2b8
Chandresh Gohel <chandreshgohel26@gmail.com>
2021-03-03 11:39:02 +0530

    Removed Copy of Project

--- BankManagementSystem_OnlineBanking/UserLogin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-from . import views
-
-urlpatterns=[
-
-    path('',views.index,name='index'),
-    path('UserRegistration',views.register,name='register')
-]
--- BankManagementSystem_OnlineBanking/UserLogin/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request,'index.html')
-
-
-def register(request):
-    return render(request,'UserRegistration.html')
